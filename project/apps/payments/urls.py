02ace561401d0b9c2f413f739e5415b7a887bd3b
amir <amiri.amir30@yahoo.com>
2020-09-15 10:51:21 +0430

    structure reverted

--- project/apps/payments/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import bazaar_token_view, GetBankView, VerifyView
-
-urlpatterns = [
-    path('bazaar-token/', bazaar_token_view, name='bazaar-token'),
-    path('gateway-bank/<int:order_id>/', GetBankView.as_view(), name='bank-gateway'),
-    path('verify/', VerifyView.as_view(), name='verify-payment'),
-
-]
--- project/apps/services/api/permissions.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class ServicePermission(BasePermission):
-    def has_permission(self, request, view):
-        return 'service' in request.auth
