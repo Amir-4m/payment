c02d0a580aaae34bac12e70e725a373162538a9c
amir <amiri.amir30@yahoo.com>
2020-09-19 10:04:49 +0430

    cov file included

--- apps/services/models.py
+++ apps/services/models.py
@@ -3,12 +3,14 @@ from django.utils.translation import ugettext_lazy as _
 
 from colorful.fields import RGBColorField
 
+from .utils import random_secret_generator
+
 
 class Service(models.Model):
     created_time = models.DateTimeField(_("created time"), auto_now_add=True)
     updated_time = models.DateTimeField(_("updated time"), auto_now=True)
     name = models.CharField(_('name'), max_length=120, null=False, blank=False)
-    secret_key = models.CharField(_('secret key'), max_length=50, null=False, blank=False, unique=True)
+    secret_key = models.CharField(_('secret key'), max_length=64, default=random_secret_generator, unique=True)
     logo = models.ImageField(_('logo'), upload_to='services/images')
     color = RGBColorField(_('color'), blank=True)
     gateways = models.ManyToManyField('payments.Gateway', related_name='services')
